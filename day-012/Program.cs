852af82422ac4e27571e5f53b1198d3f35037121
Kim Jones <43219689+klickyfan@users.noreply.github.com>
2021-06-26 22:12:36 -0400

    Add files via upload

---
+++ day-012/Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        string input = File.ReadAllText("input.json");
+
+        Part1(input);
+
+        Part2(input);
+    }
+
+    private static void Part1(string input)
+    {
+        string pattern = @"(-?\d+)";
+        RegexOptions options = RegexOptions.Multiline;
+
+        long sum = 0;
+        foreach (Match m in Regex.Matches(input, pattern, options))
+        {
+            sum += Convert.ToInt32(m.Value);
+        }
+
+        Console.WriteLine($"Part 1 sum: {sum}");
+    }
+
+    private static void Part2(string input)
+    {
+        Console.WriteLine($"Part 2 sum: {Sum(JObject.Parse(input))}");
+    }
+
+    private static long Sum(JToken token)
+    {
+        switch (token)
+        {
+            case JObject o:
+                if (o.Properties()
+                        .Select(p => p.Value).OfType<JValue>()
+                        .Select(v => v.Value).OfType<string>()
+                        .Any(s => s == "red"))
+                {
+                    return 0;
+                }
+                return o.Properties().Select(p => p.Value).Sum(t => Sum(t));
+            case JArray a:
+                return a.Sum(jt => Sum(jt));
+            case JValue v:
+                return (v.Value is long) ? (long)v.Value : 0;
+        }
+
+        throw new Exception();
+    }
+}
